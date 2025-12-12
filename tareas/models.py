5541234cbc2eed085ac2c9abae72ec74f2ca193c
LuisTejero25 <latejeroce@fpvirtualaragon.es>
2025-12-12 20:56:48 +0100

    Modelo Tarea creado y migrado

--- tareas/models.py
+++ tareas/models.py
@@ -1,3 +1,13 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Tarea(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_recordatorio = models.DateTimeField()
+
+    def __str__(self):
+        return self.titulo
